7b4651bd121e6ddda94057bde667aee00ea62733
Billy Ponto <bpgponto@gmail.com>
2022-02-12 21:58:45 +0800

    landing page with tailwind 3

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./public/**/*.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '2rem',
+        lg: '40px',
+        xl: '80px'
+      }
+    },
+    extend: {
+      colors: {
+        twmindigo: '#251382',
+        twmdark: '#17132F',
+        twmgreen: '#1FD392',
+      }
+    },
+  },
+  plugins: [],
+}
